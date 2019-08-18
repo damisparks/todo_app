b6963545c24bb9b47b56e5b5f1019020ceb707a0
Damilola Omifare <omifaredammy@gmail.com>
2019-08-18 21:46:43 +0100

    feat : Add <v-app> to the App.vue file.
    * Add <v-app> to the App.vue file.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <v-app>
+    <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view />
     </div>
-    <router-view/>
-  </div>
+  </v-app>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
