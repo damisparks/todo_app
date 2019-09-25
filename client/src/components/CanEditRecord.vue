9c3097dad0eb6fc602b9821f04111b59b2c7c187
Damilola Omifare <omifaredammy@gmail.com>
2019-09-25 22:43:31 +0100

    Update component

--- client/src/components/CanEditRecord.vue
+++ client/src/components/CanEditRecord.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <v-layout row
-wrap>
-      <v-flex xs9
-class="text-left pl-4">
-<slot/>
-        <span v-if="isEditable"
-@click="$emit('onClick')">{{ title }}</span>
+    <v-layout
+row
+              wrap
+>
+      <v-flex
+xs9
+              class="text-left pl-4"
+>
+        <slot />
+        <span
+v-if="isEditable"
+              @click="$emit('onClick')"
+>{{ title }}</span>
 
         <v-text-field
           v-if="!isEditable"
@@ -18,17 +24,21 @@ class="text-left pl-4">
         </slot>
       </v-flex>
       <v-flex xs3>
-        <v-icon v-if="isEditable"
-@click="$emit('onEdit')">
-edit
-</v-icon>
-        <v-icon v-if="!isEditable"
-@click="$emit('onSave')">
-check_circle
-</v-icon>
+        <v-icon
+v-if="isEditable"
+                @click="$emit('onEdit')"
+>
+          edit
+        </v-icon>
+        <v-icon
+v-if="!isEditable"
+                @click="$emit('onSave')"
+>
+          check_circle
+        </v-icon>
         <v-icon @click="$emit('onDelete')">
-delete
-</v-icon>
+          delete
+        </v-icon>
       </v-flex>
     </v-layout>
   </div>
